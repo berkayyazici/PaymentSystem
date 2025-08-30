9fbd2cbd3f5913501cfa563917330da932d7c59e
Berkay Yazıcı <berkayyazici27@gmail.com>
2025-08-30 17:16:04 +0300

    initial commit

---
+++ PaymentCheckService/src/main/java/com/paymentcheckservice/PaymentCheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paymentcheckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCheckServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCheckServiceApplication.class, args);
+    }
+
+}
---
+++ PaymentCheckService/src/test/java/com/paymentcheckservice/PaymentCheckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentcheckservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCheckServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
